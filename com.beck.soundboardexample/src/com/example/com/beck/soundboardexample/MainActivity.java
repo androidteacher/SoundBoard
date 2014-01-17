00245e5119002e61e5e002de3a7d09aed1177d24
root <root@Skynet>
2014-01-16 23:04:15 -0600

    Initial Commit

---
+++ com.beck.soundboardexample/src/com/example/com/beck/soundboardexample/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.com.beck.soundboardexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    TextView tv;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	    tv = (TextView) findViewById(R.id.textView1);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	public void playSound(View view)
+	{
+		tv.setText("Sound");
+	}
+
+}
